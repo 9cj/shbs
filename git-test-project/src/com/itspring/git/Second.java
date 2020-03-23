44d2d4fac6debd39b77d646f09642c5378e81f1e
cj <chen21357@163.com>
2020-03-23 20:57:13 +0800

    push dev

--- git-test-project/src/com/itspring/git/Second.java
+++ git-test-project/src/com/itspring/git/Second.java
@@ -4,5 +4,7 @@ public class Second {
     public void test() {
         System.out.println("hello second");
         System.out.println("dev_branch push了");
+        System.out.println("dev_branch push了");
+
     }
 }
