fa26ddccb633511b021462914edf1d21e2b6fb98
cj <chen21357@163.com>
2020-03-23 21:00:05 +0800

    push master

--- git-test-project/src/com/itspring/git/Three.java
+++ git-test-project/src/com/itspring/git/Three.java
@@ -3,5 +3,6 @@ package com.itspring.git;
 public class Three {
     public void commit() {
         System.out.println("de commit bu push");
+        System.out.println("de commit bu push,de 合并到master来的");
     }
 }
