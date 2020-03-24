6a1ed81e792cb0d644d0b8ffc5cbb99bb265f3fd
cj <chen21357@163.com>
2020-03-24 15:57:36 +0800

    我是分支1，（只提交到本地了）合并到master

--- git-test-project/src/com/itspring/git/Dev1UpdateClass.java
+++ git-test-project/src/com/itspring/git/Dev1UpdateClass.java
@@ -2,6 +2,10 @@ package com.itspring.git;
 
 public class Dev1UpdateClass {
     public void dev1Update() {
-        System.out.println("测试master 有没有");
+        System.out.println("测试master 有没有");//注意：修改分支之后先提交 或者push，再切换分支
+    }
+
+    public void dev1MergeMaster() {
+        System.out.println("我是分支1，（只提交到本地了）合并到master");
     }
 }
